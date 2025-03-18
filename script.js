7d363bba92285accd9082a7d631886d5cb223f3e
Pedro <pedro_h_bueno@estudante.sesisenai.org.br>
2025-03-18 09:00:33 -0300

    numero 1

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ console.log("hello ,wold")
 
 let nome = "joao";
 const idade = 25;
+
 function sindicato(){
-    console.log("ola",nome);
+    console.log("ola", + nome);
+}
+
+//par ou impar
+let numero;
+
+function verificarPar(numero){
+    if(numero%2 == 0){
+        console.log("par");
+    }else{
+        console.log("impar");
+    }
+    
 }
