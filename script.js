caf488ea0b5d88479ac119ec31bbea20e4e93dd6
Pedro <pedro_h_bueno@estudante.sesisenai.org.br>
2025-03-19 21:57:04 -0300

    numero 6 - 10

--- script.js
+++ script.js
@@ -53,3 +53,56 @@ let texto;
 function contarCaracteres(texto) {
     console.log( texto.length);
 }
+
+//Acessando Propriedades de Objetos:
+let carro = {
+    modelo: "show de bola",
+    nome:"McMaqueen",
+    ano: 2008
+
+}
+console.log (carro);
+
+// Função com Parâmetros Opcionais:
+let name;
+const mensagem = "Olá!";
+
+function  mensagemPersonalizada(name,mensagem){
+
+    console.log(mensagem + name);
+}
+// Calculadora de Média:
+let num1 ;
+let num2 ;
+let num3 ;
+let media;
+
+function mediaAritimetrica(num1,num2,num3,media){
+    media = (num1 + num2 + num3)/3;
+    console.log(media);
+}
+// Laço for com Condição:
+function multiplos(){
+    for (let i = 0; i < 21 ; i++){
+        console.log(i);
+        if(i%3 == 0){
+            console.log("multiplo de 3")
+        }else{
+            console.log("não multiplo de 3")
+        }
+    }
+}
+
+// Verificação de Palavra Palíndroma:
+let palavra ;
+function verificarPalindromo(palavra){
+
+    invertida = palavra.split("").reverse().join("");
+
+    if (invertida == palavra){
+        console.log("palíndromo")
+    }else{
+        console.log("not palíndromo")
+    }
+   
+}
